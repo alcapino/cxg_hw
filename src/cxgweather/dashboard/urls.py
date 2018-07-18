aadff0a9b4930dcdc713e29706ec0d8902c67ce3
rt-saziracav <pavacarizas@gmail.com>
2018-07-18 16:50:00 +0800

    fixed weather url using city name, add city list

--- src/cxgweather/dashboard/urls.py
+++ src/cxgweather/dashboard/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from dashboard import views
 
 urlpatterns = [
+    url(r'^city/$', views.cities_list),
     url(r'^weather/$', views.index),
-    url(r'^weather/(?P<city>[\w]+)$', views.city_detail),
+    url(r'^weather/(?P<city>[\w\s]+)/$', views.city_detail),
 ]
--- src/cxgweather/dashboard/views.py
+++ src/cxgweather/dashboard/views.py
@@ -15,9 +15,11 @@ from django.views.decorators.csrf import csrf_exempt
 #     # TODO: if no token, omit email and lastname
 #     if request.method == 'GET':
 
+@csrf_exempt
 def index(request):
     return HttpResponse("YES!")
 
+@csrf_exempt
 def city_detail(request, city, country = "PH"):
     owurl = "http://api.openweathermap.org/data/2.5/weather?q=%s,%s&appid=7c28321f513df745eed19148f4f6ce55" % (city, country)
     #return HttpResponse(owurl)
@@ -25,4 +27,12 @@ def city_detail(request, city, country = "PH"):
     resp = urllib2.urlopen(req)
     struct = json.loads(resp.read())
     data = json.dumps(struct)
+    return HttpResponse(data)
+
+
+@csrf_exempt
+def cities_list(request):
+    response = "[ { \"id\": 1701668, \"name\": \"Manila\" },{ \"id\": 7521309, \"name\": \"Davao\" },{ \"id\": 1721080, \"name\": \"Cagayan de Oro\" },{ \"id\": 1695848, \"name\": \"Angeles\" },{ \"id\": 1698829, \"name\": \"Naga\" },{ \"id\": 1729564, \"name\": \"Bacolod\" },{ \"id\": 1706889, \"name\": \"Legazpi\" },{ \"id\": 1728930, \"name\": \"Baguio\" },{ \"id\": 1726280, \"name\": \"Batangas\" }]"
+    #response = "{ id: 1701668, name: 'Manila' }";
+    data = json.dumps(response)
     return HttpResponse(data)
